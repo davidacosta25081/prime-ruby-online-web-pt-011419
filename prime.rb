5ba37b7ffdecaaa6c6863cd8490ab2af6fd44664
David Acosta <davidacosta25081@yahoo.com>
2019-02-05 05:52:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,11 +2,11 @@ def prime?(num)
   if num < 0 or num == 0 or num == 1
     return false
   else
-    (2..num-1).to_a.all? do |possible_factor|
-      num % possible_factor != 0
+    x = (2..num-1).to_a
+print x
     end
   end
 end
 
 
-prime?(2)
+
